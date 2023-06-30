de7b1a28dd418072f91aa0df1975b0eb8abe84de
Gideon Mitchell <gideonm@uchicago.edu>
2023-06-30 15:41:17 -0500

    Figured out move generation from chess lib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+
 #include "board.h"
+#include "gen.h"
+
+static Board board;
+Move moves[MAX_MOVES];
 
 int main() {
-    printf("Config works");
+    char *fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    board_load_fen(&board, fen);
+    board_print(&board);
+    printf("Board Color: %d\n", board.color);
+
+    int count = gen_legal_moves(&board, moves);
+    for (int i = 0; i < count; i++) {
+        Move *move = &moves[i];
+        print_move(&board, move);
+        printf("\n");
+    }
 }
